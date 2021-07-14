02e8f23e81a85dfdc4041182d3b73256c5e2ac95
Emily Sheetz <ejsheetz@yahoo.com>
2021-07-13 22:32:21 -0500

    preliminary implementation of val utils

--- RobotSystems/Valkyrie/valkyrie_utils.hpp
+++ RobotSystems/Valkyrie/valkyrie_utils.hpp
@@ -4,6 +4,7 @@
  **/
 
 #include <map>
+#include <memory>
 #include <utility>
 
 #include <Utils/wrap_eigen.hpp>
@@ -13,7 +14,9 @@
 namespace ValUtils {
 
     void constructJointGroupMap(std::map<int, std::vector<std::pair<int, std::string>>>& joint_group_map);
-    void getStandingConfiguration(Valkyrie_Model val_model, dynacore::Vector& q, dynacore::Vector& qdot);
+
+    void getStandingConfiguration(std::shared_ptr<Valkyrie_Model> val_model, dynacore::Vector& q, dynacore::Vector& qdot);
+    
     void setIKTasksForGroup(); // TODO
 
 } // end namespace ValUtils
